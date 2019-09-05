5468a454ef39f7e9edc8043a0473a948d73f18f3
kyawbathit <kyawbathit@gmail.com>
2019-09-05 09:08:19 +0200

    add Future-Age

---
+++ javascript/javascript1/week1/Future_age.js
@@ -0,0 +1,6 @@
+//Age-ify (A future age calculator)
+const yearOfBirth = 1987;
+const yearFuture = 2027;
+const age = yearFuture - yearOfBirth;
+
+console.log ("You will be "+age+" years old in "+yearFuture+".");
