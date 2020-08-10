4b653b4fd91d6637c9028a479562636363792341
kyawbathit <kyawbathit@gmail.com>
2020-08-10 12:38:17 +0200

    my week2 homework

---
+++ javascript/javascript1/week2/full-name.js
@@ -0,0 +1,5 @@
+function getFullName (firstName, surName){
+    return firstName +" "+ surName;
+}
+const fullName1 = getFullName("Benjamin", "Hughes");
+const fullName2 = getFullName("Alex", "Cooper");
---
+++ javascript/javascript1/week2/get-full-name.js
@@ -0,0 +1,27 @@
+function getFullName (firstName, surName) {
+    return firstName + " " + surName;
+}
+    console.log (getFullName("Kyaw", "Minnhla"));
+
+// const fullName1 = getFullName (true);
+// const fullName2 = getFullName (false);
+
+function $getFullname (getFullName, useFormalName) {
+   if (getFullName === "") {
+   return "Lord" + " " + getFullName;
+} else {
+ return  getFullName;
+}
+}
+console.log (getFullName);
+// console.log ();
+//     if (useFormalName === "") {
+//     return "${firstName} ${surName}";
+// } else {
+//     return "Lord ${firstName} ${surname}";
+// }
+// }
+// const fullName1 = getFullName ("Kyaw", "Minnhla", true);
+// const fullName2 = getFullName ("Kyaw", "Minnhla", false);
+// console.log (fullName1);
+// console.log (fullName2);
