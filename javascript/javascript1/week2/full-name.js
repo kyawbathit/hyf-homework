1da1fc01eaef681fc79381624e06740cff6f59d3
Kjaw Minn Hla <51692201+kyawbathit@users.noreply.github.com>
2020-08-10 12:43:56 +0200

    Delete full-name.js

--- javascript/javascript1/week2/full-name.js
+++
@@ -1,5 +0,0 @@
-function getFullName (firstName, surName){
-    return firstName +" "+ surName;
-}
-const fullName1 = getFullName("Benjamin", "Hughes");
-const fullName2 = getFullName("Alex", "Cooper");
