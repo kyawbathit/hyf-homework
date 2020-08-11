cb796c5754f1c2547214bf407cc125b71d1fbf76
kyawbathit <kyawbathit@gmail.com>
2020-08-11 08:49:06 +0200

    Add EventPlanner

---
+++ javascript/javascript1/week2/EventPlanner.js
@@ -0,0 +1,11 @@
+//Event Planner 
+const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+function getEventWeekday (eventDay){
+    const currentDate = new Date();
+    const currentDay = currentDate.getDay();
+    let i = (currentDay + eventDay) % 7;
+    return weekday[i];
+}
+console.log(getEventWeekday(9));
+console.log(getEventWeekday(3));
