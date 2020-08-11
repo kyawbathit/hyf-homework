5bc9aeacfd4f4c40e40467cf41c68bf6673a7c87
kyawbathit <kyawbathit@gmail.com>
2020-08-11 09:41:34 +0200

    Add Weather Wear

---
+++ javascript/javascript1/week2/WeatherWear.js
@@ -0,0 +1,13 @@
+//Weather To Wear
+const clothes = ["Short and T-shirt", "Jeans and Raincoat", "Tight inside and Fur Coat"];
+function chooseWithWeather (temperature){
+  if (temperature <= 15) {
+    return clothes[2];
+  } else if (temperature > 15 && temperature <= 25) {
+    return clothes[1];
+  } else {
+    return clothes[0];
+  }
+}
+const clothesToWear = chooseWithWeather(20);
+console.log(clothesToWear); //'Jeans and Raincoat'
