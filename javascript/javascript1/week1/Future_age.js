09354c5882e9f79fc9af72245df1b3ad9e8fbe7b
kyawbathit <kyawbathit@gmail.com>
2020-08-17 12:05:14 +0200

    Edited las- year homework

--- javascript/javascript1/week1/Future_age.js
+++ javascript/javascript1/week1/Future_age.js
@@ -3,4 +3,4 @@ const yearOfBirth = 1987;
 const yearFuture = 2027;
 const age = yearFuture - yearOfBirth;
 
-console.log ("You will be "+age+" years old in "+yearFuture+".");
+console.log (`You will be ${age} years old in ${yearFuture}.`);
