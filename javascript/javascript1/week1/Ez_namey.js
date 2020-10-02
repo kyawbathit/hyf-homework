d7d4c325189e187d3e692c8909f525cde4e1d6cc
kyawbathit <kyawbathit@gmail.com>
2020-10-02 10:47:26 +0200

    edited

--- javascript/javascript1/week1/Ez_namey.js
+++ javascript/javascript1/week1/Ez_namey.js
@@ -1,6 +1,16 @@
 //Ez Namey (Startup name generator)
 const firstWords = ["Hoohle", "Zahoo", "Nozilla", "Fakebook", "Tweeter", "See", "Tee", "Vee","Xee", "Zee"]
-const lastWords = ["Corp", "Group", "Company", "Limited", "Inc", "Org", "Network", "Associate", "Firm", "Society"];
-const startUpName = firstWords[Math.floor(Math.random() * 10)] +" "+ lastWords[Math.floor(Math.random() * 10)];
+const secondWords = ["Corp", "Group", "Company", "Limited", "Inc", "Org", "Network", "Associate", "Firm", "Society"];
+const randomNumber = Math.floor(Math.random() * 10) + 0;
+const startUpName = firstWords[randomNumber] +" "+ secondWords[randomNumber];
 
 console.log ("The startup: \""+startUpName+"\" containing "+startUpName.length+" characters.");
+
+//*** in a function ***//
+
+// function genStartUp () {
+//   const randomNumber = Math.floor(Math.random() * 10) + 0;
+//   const startUpName = firstWords[randomNumber] +" "+ secondWords[randomNumber];
+//   return startUpName +": contains "+ startUpName.length +" chars";
+//  };
+// console.log(genStartUp())
