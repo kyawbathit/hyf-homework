ea645e17e77d3f81d6650c604d9f824b4fba74de
kyawbathit <kyawbathit@gmail.com>
2020-10-02 09:43:59 +0200

    edited

--- javascript/javascript1/week1/Dog_age.js
+++ javascript/javascript1/week1/Dog_age.js
@@ -1,12 +1,12 @@
 //Goodboy-Oldboy (A dog age calculator)
-const yearOfBirth = 2020;
-const yearFuture = 2027;
-const humanAge = yearFuture - yearOfBirth;
-const dogAge = (yearFuture - yearOfBirth) * 7;
+const dogYearOfBirth = 2020;
+const dogYearFuture = 2027;
+const humanYear = dogYearFuture - dogYearOfBirth;
+const dogYear = (dogYearFuture - dogYearOfBirth) * 10;
 const shouldShowResultInDogYears = false;
 
 if (shouldShowResultInDogYears === true) {
-    console.log ("Your dog will be "+dogAge+" dog years old in "+dogYearFuture+".")
-} else {
-console.log ("Your dog will be "+humanAge+" human years old in "+yearFuture+".");
+    console.log (`Your dog will be ${dogYear} dog years old in ${dogYearFuture}.`)
+    } else {
+    console.log (`Your dog will be ${humanYear} human years old in ${dogYearFuture}.`);
 };
