b5c60ba586b9afb61d4dee2a530d83645eee26f4
kyawbathit <kyawbathit@gmail.com>
2020-11-25 19:31:52 +0100

    added find characters

--- javascript/javascript2/week1/ShortestWord.js
+++ javascript/javascript2/week1/ShortestWord.js
@@ -8,3 +8,16 @@ const danishWords = ['bil', 'plante', 'kaffe', 'bog', 'ø', 'planetarium'];
 console.log(findShortestWord(danishWords)); // 'ø'
 
 //find Characters
+const danishString = "Jeg har en blå bil";
+const danishString2 = "Blå grød med røde bær";
+function findChar (str) {
+  const searchedChar = /[åøæ]/gi;
+  let chars = {};
+   chars.total = (str.match(searchedChar) == null) ? 0
+   : str.match(searchedChar).length; 
+  str.replace(searchedChar, function(l){chars[l] = (isNaN(chars[l]) ? 1 : 
+    chars[l] + 1);});
+    return chars;
+}
+findChar(danishString); //{ total: 1, 'å': 1 }
+findChar(danishString2); //{ total: 4, 'å': 1, 'ø': 2, 'æ': 1 }
