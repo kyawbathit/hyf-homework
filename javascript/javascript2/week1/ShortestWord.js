bda2d938d3b47756ec31150996441285a2a48fb8
kyawbathit <kyawbathit@gmail.com>
2020-11-25 09:41:10 +0100

    deleted files

--- javascript/javascript2/week1/ShortestWord.js
+++
@@ -1,8 +0,0 @@
-// //The shortest Word
-const danishWords = ['bil', 'plante', 'kaffe', 'bog', 'ø', 'planetarium'];
-  function findShortestWord (arr) {
-    return arr.reduce(function (a, b){
-      return a.length <= b.length ? a : b;
-});
-}
-console.log(findShortestWord(danishWords));
