26d4d76c9df585f8c77094585cac96ddb1895fe9
kyawbathit <kyawbathit@gmail.com>
2020-11-25 20:04:20 +0100

    added

---
+++ javascript/javascript2/week1/ShortestWord.js
@@ -0,0 +1,23 @@
+//The shortest Word
+const danishWords = ['bil', 'plante', 'kaffe', 'bog', 'ø', 'planetarium'];
+  function findShortestWord (arr) {
+    return arr.reduce((a, b) =>
+    a.length <= b.length ? a : b);
+}
+
+console.log(findShortestWord(danishWords)); // 'ø'
+
+//find Characters
+const danishString = "Jeg har en blå bil";
+const danishString2 = "Blå grød med røde bær";
+function findChar (str) {
+  const searchedChar = /[åøæ]/gi;
+  let chars = {};
+   chars.total = (str.match(searchedChar) == null) ? 0
+   : str.match(searchedChar).length; 
+  str.replace(searchedChar, function(l){chars[l] = (isNaN(chars[l]) ? 1 : 
+    chars[l] + 1);});
+    return chars;
+}
+console.log(findChar(danishString)); //{ total: 1, 'å': 1 }
+console.log(findChar(danishString2)); //{ total: 4, 'å': 1, 'ø': 2, 'æ': 1 }
