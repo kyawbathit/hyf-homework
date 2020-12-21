95ebfd78840543e7e061a3630fa930dd3bd21b9e
kyawbathit <kyawbathit@gmail.com>
2020-12-21 10:54:35 +0100

    edited fastest JS

--- javascript/javascript2/week3/fastest.js
+++ javascript/javascript2/week3/fastest.js
@@ -13,8 +13,7 @@ function countSeconds (e) {
     } else {
         clicked = true;
     }
-    countKeyPress (e); 
-    setTimer(second);
+      setTimer(second);
 } 
 
 function setTimer(second) {
@@ -22,12 +21,14 @@ function setTimer(second) {
       second--;
       if (second > 0) {
         para.innerHTML = second; 
-      } else if(second === 0){
+      } else if (second === 0){
+        input.value = "";
         para.innerHTML = "Time's up! Restart"; 
         btn.innerText = 'Restart game';
        }
     }, 1000);
   };
+
 
 const scoreL = document.getElementById("score-l");
 const scoreS = document.getElementById("score-s");
@@ -35,23 +36,21 @@ document.addEventListener("keypress", countKeyPress);
 
 function countKeyPress (e) {
     const keyValue = e.key;
-       if (clicked && keyValue === "l") {
+       if (input.value > 0 && keyValue === "l") {
       countL = parseInt(scoreL.textContent || 0);
       scoreL.textContent = countL + 1;
-    } else if (clicked && keyValue === "s") {
+    } else if (input.value > 0 && keyValue === "s") {
       countS = parseInt(scoreS.textContent || 0);
       scoreS.textContent = countS + 1;
+    } else {
+      return;
     }
-    return;
 }
 
 const winnerL = document.getElementById("win-l");
 const winnerS = document.getElementById("win-s");
 const getResult = ()  => {
-    if (scoreS.innerHTML === "" && scoreL.innerHTML === "" && second === "") {
-      winnerL.textContent = "";
-      winnerS.textContent = "";
-    } else if (scoreS.innerHTML === "" && scoreL.innerHTML === "") {
+      if (input.value > 0 && scoreS.innerHTML === "" && scoreL.innerHTML === "") {
       scoreL.textContent = "Press the 'l' key";
       scoreS.textContent = "Press the 's' key";
     } else if (scoreS.innerHTML > scoreL.innerHTML) {
@@ -61,12 +60,20 @@ const getResult = ()  => {
     } else if (scoreS.innerHTML === scoreL.innerHTML) {
       winnerL.textContent = "It is draw";
       winnerS.textContent = "It is draw";
-    }
-    
+    } 
 };
 
-const timeout = () => setTimeout(getResult, second * 1000)
+const timeout = () => setTimeout(getResult, second * 1000);
 btn.addEventListener("click", timeout);
+btn.addEventListener('click', clearResult, false);
+function clearResult () {
+  if (btn.innerText == 'Restart game') {
+    scoreS.textContent = "";
+    scoreL.textContent = "";
+    winnerL.textContent = "";
+    winnerS.textContent = "";
+}
+}
 
 
 
