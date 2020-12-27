602febc67b2244ca4d301ad3d02df8bc1d4829aa
kyawbathit <kyawbathit@gmail.com>
2020-12-27 17:02:11 +0100

    edited some

--- javascript/javascript3/week2/promise-visual/main.js
+++ javascript/javascript3/week2/promise-visual/main.js
@@ -1,7 +1,8 @@
 //Visual Promise
+//One By One
 const span = document.querySelector('span');
-function translateOneByOne(nthChild, x, y){
-    return new Promise (resolve => {
+async function translateOneByOne(nthChild, x, y){
+    return await new Promise (resolve => {
         moveElement(document.querySelector(`li:nth-child(${nthChild})`), { 
             x, 
             y 
@@ -12,22 +13,23 @@ function translateOneByOne(nthChild, x, y){
         });
     })
 }
-//One By One
-// translateOneByOne(1, 20, 300)
-// .then(() => translateOneByOne(2, 400, 300))
-// .then(() => translateOneByOne(3, 400, 20))
-// .then (() => span.setAttribute('class', 'shown'))
+
+translateOneByOne(1, 20, 300)
+.then(() => translateOneByOne(2, 400, 300))
+.then(() => translateOneByOne(3, 400, 20))
+.then (() => span.setAttribute('class', 'shown'))
 
 //All at Once
-async function translateAllAtOnce(){
-    const moveRed = translateOneByOne(1, 20, 300);
-    const moveBlue = translateOneByOne(2, 400, 300);
-    const moveGreen = translateOneByOne(3, 400, 20);
-      await Promise.all([moveRed, moveBlue, moveGreen])
-        .then (() => {
-            console.log("Elements have been moved");
-            span.setAttribute('class', 'shown')
-        })
+
+// function translateAllAtOnce(){
+//     const moveRed = moveElement(document.querySelector('li:nth-child(1)'), {x: 20, y: 300});
+//     const moveBlue = moveElement(document.querySelector('li:nth-child(2)'), {x: 400, y: 300});
+//     const moveGreen = moveElement(document.querySelector('li:nth-child(3)'), {x: 400, y: 20});
+//       Promise.all([moveRed, moveBlue, moveGreen])
+//         .then (() => {
+//             console.log("Elements have been moved");
+//             span.setAttribute('class', 'shown')
+//         })
     
-}
-translateAllAtOnce()
+// }
+// translateAllAtOnce()
